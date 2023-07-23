7ba1ba2c9ee9d20f51c011d962899ef6807e0b1a
Pratham <mishrapratham.codeit@gmail.com>
2023-07-23 19:58:22 +0530

    v0.0.8

--- core/pre_utilities.py
+++ core/pre_utilities.py
@@ -1,24 +1,83 @@
+import xml.etree.ElementTree as ET
 
-def titleExtractor():
+def titleExtractor(block_xml):
+    if(block_xml.find('w:pStyle w:val="Title"') != -1):
+        if(block_xml.find('<w:t') != -1):
+            return f"Is a title. text={textExtractor(block_xml)}"
+    return None
+
+def subtitleExtractor(block_xml):
+    if(block_xml.find('w:pStyle w:val="Subtitle"') != -1):
+        if(block_xml.find('<w:t') != -1):
+            return f"Is a subtitle. text={textExtractor(block_xml)}"
+    return None
+
+def headExtractor(block_xml):
+    if(block_xml.find('w:pStyle w:val="Heading1"') != -1):
+        if(block_xml.find('<w:t') != -1):
+            return f"Is a heading1. text={textExtractor(block_xml)}"
+    elif(block_xml.find('w:pStyle w:val="Heading2"') != -1):
+        if(block_xml.find('<w:t') != -1):
+            return f"Is a heading2. text={textExtractor(block_xml)}"
+    elif(block_xml.find('w:pStyle w:val="Heading3"') != -1):
+        if(block_xml.find('<w:t') != -1):
+            return f"Is a heading3. text={textExtractor(block_xml)}"
+    elif(block_xml.find('w:pStyle w:val="Heading4"') != -1):
+        if(block_xml.find('<w:t') != -1):
+            return f"Is a heading4. text={textExtractor(block_xml)}"
+    elif(block_xml.find('w:pStyle w:val="Heading5"') != -1):
+        if(block_xml.find('<w:t') != -1):
+            return f"Is a heading5. text={textExtractor(block_xml)}"
+    elif(block_xml.find('w:pStyle w:val="Heading6"') != -1):
+        if(block_xml.find('<w:t') != -1):
+            return f"Is a heading6. text={textExtractor(block_xml)}"
+    return None
+
+def paraNimageExtractor(block_xml):
+    if(block_xml.find('<w:pStyle ') == -1 and block_xml.find('<w:tblStyle ') == -1):
+        if imageExtractor(block_xml) is not None:
+            return imageExtractor(block_xml)
+        else:
+            if(block_xml.find('<w:t') != -1):
+                return f'Is a paragraph. text={textExtractor(block_xml)})'
+            else:
+                if(block_xml.startswith('<w:sectPr ') == True):
+                    return 'Is a section break.'
+                else:
+                    return 'Is a white line.'
+def listExtractor(block_xml):
+    if(block_xml.find('w:pStyle w:val="ListParagraph"') != -1):
+        if(block_xml.find('<w:t') != -1):
+            return f"Is a list. text={textExtractor(block_xml)}"
+    return None
+
+def tableExtractor(block_xml):
+    if(block_xml.find('<w:tblStyle w:val="TableGrid"/>') != -1):
+        return "Is a table."
+    return None
+
+def imageExtractor(block_xml):
+    if(block_xml.find('<w:drawing>') != -1):
+        return "Is an image."
+    return None
+
+def textExtractor(block_xml):
+    root = ET.fromstring(block_xml)
+    text_values = [t.text for t in root.findall('.//{http://schemas.openxmlformats.org/wordprocessingml/2006/main}t')]
+    result_string = " ".join(text_values)
+    return result_string
+
+def tableDataConfig():
     pass
 
-def subheadingExtractor():
+def imageBase64DataConfig():
     pass
 
-def headExtractor():
+def boldPartExtractor():
     pass
 
-def paraExtractor():
+def italicsPartExtractor():
     pass
 
-def listExtractor():
-    pass
-
-def tableExtractor():
-    pass
-
-def imageExtractor():
-    pass
-
-def linkExtractor():
+def innerTxtExtractorStyleConfig():
     pass
--- core/utilities.py
+++ core/utilities.py
@@ -3,7 +3,30 @@ import streamlit as st
 from core.pre_utilities import *
 
 def config(document):
-    return 'in progress'
+    block_xmlArr = []
+    lineDict = {}
+    for block in document.element.body:
+        block_xmlArr.append(block.xml)
+
+    for iteration in range(1, len(block_xmlArr) + 1):
+        if titleExtractor(block_xmlArr[iteration - 1]) != None:
+            lineDict[f"Line {iteration}"] = titleExtractor(block_xmlArr[iteration - 1])
+        elif subtitleExtractor(block_xmlArr[iteration - 1]) != None:
+            lineDict[f"Line {iteration}"] = subtitleExtractor(block_xmlArr[iteration - 1])
+        elif headExtractor(block_xmlArr[iteration - 1]) != None:
+            lineDict[f"Line {iteration}"] = headExtractor(block_xmlArr[iteration - 1])
+        elif listExtractor(block_xmlArr[iteration - 1]) != None:
+            lineDict[f"Line {iteration}"] = listExtractor(block_xmlArr[iteration - 1])
+        elif tableExtractor(block_xmlArr[iteration - 1]) != None:
+            lineDict[f"Line {iteration}"] = tableExtractor(block_xmlArr[iteration - 1])
+        elif paraNimageExtractor(block_xmlArr[iteration - 1]) != None:
+            lineDict[f"Line {iteration}"] = paraNimageExtractor(block_xmlArr[iteration - 1])
+    
+    temp = ""
+    for content in lineDict:
+        temp = temp + str(f"{content} : {lineDict[content]} \n\n")
+
+    return temp
 
 def renderer():
     uploaded_file = st.file_uploader("Choose a .docx file")
